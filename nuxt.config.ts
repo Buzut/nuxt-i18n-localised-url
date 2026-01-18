676cbdff7d2b92cbbb59623adec4340b669d940a
Buzut <quentin@buzut.net>
2026-01-18 13:57:50 +0100

    setup cloudflare config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['nuxt-i18n-micro'],
+  ssr: true,
 
   i18n: {
     locales: [
@@ -13,7 +14,37 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
     translationDir: 'locales',
     meta: true,
-    debug: true,
+    // debug: true,
     disablePageLocales: true
+  },
+
+  routeRules: {
+    // Cache _nuxt assets for 1 year (JS, CSS, images)
+    // '/_nuxt/**': {
+    //   headers: { 'cache-control': 'public, max-age=31536000, s-maxage=31536000, immutable' }
+    // },
+
+    '/**': {
+      swr: 3600,
+      headers: { 'cache-control': 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600' }
+    }
+  },
+
+  nitro: {
+    preset: 'cloudflare_module',
+
+    prerender: {
+      ignore: [
+        '/products/:slug(.*)*',
+        '/fr/produits/:slug(.*)*'
+      ]
+    },
+
+    storage: {
+      cache: {
+        driver: 'cloudflare-kv-binding',
+        binding: 'WEB_CACHE'
+      }
+    }
   }
 })
